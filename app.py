ae91c8994d3edc206926403bf9d8ff0b44f93227
kambooosss <89924142+kambooosss@users.noreply.github.com>
2021-09-11 12:53:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,41 @@
+pip install adafruit-io
+
+from Adafruit_IO import Client
+aio = Client('kambooosss','aio_MAJQ44YXuPXas0AN4hdY3TN0BTHW')
+
+pip install python-telegram-bot==13.0 --quiet
+
+
+
+
+
+from telegram.ext import Updater, MessageHandler, Filters
+
+
+
+
+def demo1(bot,update):
+  chat_id = bot.message.chat_id
+  bot.message.reply_text('turning on')
+  aio.send('light',1)
+  
+def demo2(bot,update):
+  chat_id = bot.message.chat_id
+  bot.message.reply_text('turning off')
+  aio.send('light',900)
+
+def main(bot,update):
+  a= bot.message.text.lower()
+  if a =="on":
+    demo1(bot,update)
+  elif a =="off":
+    demo2(bot,update)
+
+
+bot_token = '1959189837:AAHYhYl4nvq7vXGFtVWoP7ZgQWY0wB_VTtc'
+u = Updater(bot_token,use_context=True)
+dp = u.dispatcher
+dp.add_handler(MessageHandler(Filters.text,main))
+u.start_polling()
+u.idle()
 
