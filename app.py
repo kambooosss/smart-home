215f973d361d143f1ffd82ba78fd0eeb2e06454c
kambooosss <89924142+kambooosss@users.noreply.github.com>
2021-09-14 07:56:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,21 +20,34 @@ from telegram.ext import Updater, MessageHandler, Filters
 
 def demo1(bot,update):
   chat_id = bot.message.chat_id
-  bot.message.reply_text('turning on')
+  bot.message.reply_text('turning on light')
   aio.send('light',1)
   
 def demo2(bot,update):
   chat_id = bot.message.chat_id
-  bot.message.reply_text('turning off')
-  aio.send('light',900)
+  bot.message.reply_text('turning off light')
+  aio.send('light',0)
+
+def demo3(bot,update):
+  chat_id = bot.message.chat_id
+  bot.message.reply_text('turning on fan')
+  aio.send('fan',1)
+
+def demo4(bot,update):
+  chat_id = bot.message.chat_id
+  bot.message.reply_text('turning off fan')
+  aio.send('fan',0)
 
 def main(bot,update):
   a= bot.message.text.lower()
-  if a =="on":
+  if a =="turn on light":
     demo1(bot,update)
-  elif a =="off":
+  elif a =="turn off light":
     demo2(bot,update)
-
+  elif a =="turn on fan":
+    demo3(bot,update)
+  elif a =="turn off fan":
+    demo4(bot,update)
 
 bot_token = f'{token1}:{token2}'
 u = Updater(bot_token,use_context=True)
